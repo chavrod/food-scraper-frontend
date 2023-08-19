274075c8096bc039578d8c638366ff15f89e4018
Dima Chavro <dchavro@gmail.com>
2023-08-19 16:36:05 +0100

    Distinguish between relevant and all product types

--- sw_core/core/admin.py
+++ sw_core/core/admin.py
@@ -3,5 +3,6 @@ import core.models as core_models
 
 # Register your models here.
 
+admin.site.register(core_models.CachedRelevantProduct)
+admin.site.register(core_models.CachedAllProduct)
 admin.site.register(core_models.CachedProductsPage)
-admin.site.register(core_models.Product)
--- sw_core/core/models.py
+++ sw_core/core/models.py
@@ -17,7 +17,17 @@ class ShopPageCount(models.IntegerChoices):
     SUPERVALU = 30
 
 
-class Product(models.Model):
+class CachedRelevantProduct(models.Model):
+    name = models.CharField(max_length=100)
+    shop = models.CharField(max_length=30, choices=ShopName.choices)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return f"{self.shop}: {self.name} {self.price}"
+
+
+class CachedAllProduct(models.Model):
     name = models.CharField(max_length=100)
     shop = models.CharField(max_length=30, choices=ShopName.choices)
     price = models.DecimalField(max_digits=10, decimal_places=2)
@@ -31,7 +41,8 @@ class CachedProductsPage(models.Model):
     query = models.CharField(max_length=30)
     page = models.IntegerField(validators=[MinValueValidator(1)])
     results = models.JSONField(default=dict)
-    relevant_only = models.BooleanField(default=True)
+    is_relevant_only = models.BooleanField(default=True)
+    is_ascending = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
