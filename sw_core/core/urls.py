174a6785b6339a0c6e5ed3385e75e684cb9b37d7
Dima Chavro <dchavro@gmail.com>
2023-09-07 15:43:19 +0100

    Impelemt backend logout

--- sw_core/core/urls.py
+++ sw_core/core/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("register/", core_views.RegisterView.as_view()),
     path("login/", core_views.LoginView.as_view()),
     path("user/", core_views.UserView.as_view()),
+    path("logout/", core_views.LogoutView.as_view()),
     path(
         "cached_products_page/",
         core_views.CachedProductsPageViewSet.as_view({"get": "retrieve"}),
