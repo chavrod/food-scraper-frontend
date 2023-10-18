98fc891cd2e0d7fa03d25b47093ea0d675d66417
Dima Chavro <dchavro@gmail.com>
2023-10-18 17:47:33 +0100

    email and ip blacklistt models

--- sw_core/authentication/models.py
+++ sw_core/authentication/models.py
@@ -5,7 +5,13 @@ class EmailRequestBlacklist(models.Model):
     email = models.EmailField(unique=True)
     request_count = models.IntegerField(default=0)
 
+    def __str__(self) -> str:
+        return f"{self.email}: {self.request_count}"
+
 
 class IPRequestBlacklist(models.Model):
     ip_address = models.GenericIPAddressField(unique=True)
     request_count = models.IntegerField(default=0)
+
+    def __str__(self) -> str:
+        return f"{self.ip_address}: {self.request_count}"
