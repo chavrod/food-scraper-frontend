c34f9b89ab20b336a3333381af6af41d960f1dd6
Dima Chavro <dchavro@gmail.com>
2023-11-11 15:24:59 +0000

    add grey background

--- sw_customer/app/layout.tsx
+++ sw_customer/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html>
       <body>
-        <main>
+        <main style={{ backgroundColor: "#F1F3F5" }}>
           <RootStyleRegistry>
             <SessionProvider>
               <MainAppShell>{children}</MainAppShell>
