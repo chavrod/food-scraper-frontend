b0382f92586a3cfbd09693c079d12ee7c4eeee8a
Dima Chavro <dchavro@gmail.com>
2023-11-13 18:26:41 +0000

    Add tabs

--- sw_customer/Components/SearchResults.tsx
+++ sw_customer/Components/SearchResults.tsx
@@ -19,8 +19,15 @@ import {
   Box,
   Space,
   Title,
+  Tabs,
+  Badge,
 } from "@mantine/core";
-import { IconArrowBadgeRight, IconShoppingBagPlus } from "@tabler/icons-react";
+import {
+  IconArrowBadgeRight,
+  IconShoppingBagPlus,
+  IconSearch,
+  IconShoppingCart,
+} from "@tabler/icons-react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 // Intenral: Utils
 import { Product, SearchMetaData } from "@/utils/types";
@@ -179,99 +186,93 @@ export default function SearchResults({
   }, [currentProducts, currentAverageScrapingTime]);
 
   return (
-    <Stack align="center">
-      <form onSubmit={form.onSubmit(handleFormSubmit)}>
-        <Group mb="md">
-          <TextInput
-            id="1"
-            withAsterisk
-            placeholder="Type a product name"
-            disabled={loadingStates.loading}
-            {...form.getInputProps("query")}
-          />
-          <Button type="submit" loading={loadingStates.loading}>
-            Search
-          </Button>
-        </Group>
-      </form>
-      {loadingStates.loading &&
-      loadingStates.loadingNew &&
-      !loadingStates.loadingCached ? (
-        <Paper shadow="md" radius="md" p="md">
-          <Stack mt={20} mb={5} align="center">
-            <Title align="center" w={300}>
-              {" "}
-              Searching supermarkets...
-            </Title>
-            {currentAverageScrapingTime && (
-              <Text align="center" color="dimmed" w={300}>
-                Hang tight! We're checking supermarkets for up-to-date product
-                data. So far, this has taken us{" "}
-                {Math.ceil(currentAverageScrapingTime) + 10} seconds on average.
-              </Text>
-            )}
+    <Tabs defaultValue="gallery">
+      <Title align="left" mb="sm">
+        Explore
+      </Title>
+      <Text c="dimmed" mb="md">
+        Search for products, add to basket, and make your choices
+      </Text>
+      <Tabs.List>
+        <Tabs.Tab value="search" icon={<IconSearch size="1.2rem" />}>
+          Search
+        </Tabs.Tab>
+        <Tabs.Tab
+          value="basket"
+          icon={<IconShoppingCart size="1.2rem" />}
+          rightSection={
+            <Badge
+              w={21}
+              h={21}
+              sx={{ pointerEvents: "none" }}
+              variant="filled"
+              size="xs"
+              p={0}
+            >
+              12
+            </Badge>
+          }
+        >
+          Basket
+        </Tabs.Tab>
+      </Tabs.List>
 
-            {currentAverageScrapingTime && (
-              <CountdownCircleTimer
-                isPlaying={
-                  loadingStates.loading &&
-                  loadingStates.loadingNew &&
-                  !loadingStates.loadingCached
-                }
-                duration={Math.ceil(currentAverageScrapingTime) + 5}
-                colors={["#0C8599", "#15AABF", "#0CA678", "#37B24D"]}
-                colorsTime={[10, 7, 4, 0]}
-                strokeWidth={20}
-              >
-                {renderTime}
-              </CountdownCircleTimer>
-            )}
-          </Stack>
-        </Paper>
-      ) : loadingStates.loading &&
-        !loadingStates.loadingNew &&
-        loadingStates.loadingCached ? (
-        <Grid gutter="md" justify="center">
-          {Array.from({ length: 24 }).map((_, index) => (
-            <Grid.Col key={index} span={12} md={6} xl={4}>
-              <Paper h="190px" shadow="md" withBorder p="sm" m="xs" radius="md">
-                <Flex
-                  gap="md"
-                  justify="flex-start"
-                  align="flex-start"
-                  direction="row"
-                  wrap="nowrap"
-                >
-                  <Stack align="center">
-                    <Skeleton height={80} width={80} />
-                    <Skeleton height={60} width={60} />
-                  </Stack>
+      <Tabs.Panel value="search" pt="xs">
+        <Stack align="center">
+          <form onSubmit={form.onSubmit(handleFormSubmit)}>
+            <Group mb="md">
+              <TextInput
+                id="1"
+                withAsterisk
+                placeholder="Type a product name"
+                disabled={loadingStates.loading}
+                {...form.getInputProps("query")}
+              />
+              <Button type="submit" loading={loadingStates.loading}>
+                Search
+              </Button>
+            </Group>
+          </form>
+          {loadingStates.loading &&
+          loadingStates.loadingNew &&
+          !loadingStates.loadingCached ? (
+            <Paper shadow="md" radius="md" p="md">
+              <Stack mt={20} mb={5} align="center">
+                <Title align="center" w={300}>
+                  {" "}
+                  Searching supermarkets...
+                </Title>
+                {currentAverageScrapingTime && (
+                  <Text align="center" color="dimmed" w={300}>
+                    Hang tight! We're checking supermarkets for up-to-date
+                    product data. So far, this has taken us{" "}
+                    {Math.ceil(currentAverageScrapingTime) + 10} seconds on
+                    average.
+                  </Text>
+                )}
 
-                  <Stack
-                    style={{
-                      width: "80%",
-                    }}
+                {currentAverageScrapingTime && (
+                  <CountdownCircleTimer
+                    isPlaying={
+                      loadingStates.loading &&
+                      loadingStates.loadingNew &&
+                      !loadingStates.loadingCached
+                    }
+                    duration={Math.ceil(currentAverageScrapingTime) + 5}
+                    colors={["#0C8599", "#15AABF", "#0CA678", "#37B24D"]}
+                    colorsTime={[10, 7, 4, 0]}
+                    strokeWidth={20}
                   >
-                    <Skeleton height={8} width={200} />
-                    <Skeleton height={8} width={160} />
-                    <Skeleton height={8} width={100} />
-                    <Space h={35} />
-                    <Group position="apart">
-                      <Skeleton height={25} width={35} />
-                      <Skeleton height={30} width={80} radius="lg" />
-                    </Group>
-                  </Stack>
-                </Flex>
-              </Paper>
-            </Grid.Col>
-          ))}
-        </Grid>
-      ) : (
-        currentProducts &&
-        currentProducts.length > 0 && (
-          <Stack align="center" spacing={0}>
-            <Grid gutter={0} justify="center">
-              {currentProducts.map((product, index) => (
+                    {renderTime}
+                  </CountdownCircleTimer>
+                )}
+              </Stack>
+            </Paper>
+          ) : loadingStates.loading &&
+            !loadingStates.loadingNew &&
+            loadingStates.loadingCached ? (
+            <Grid gutter="md" justify="center">
+              {Array.from({ length: 24 }).map((_, index) => (
                 <Grid.Col key={index} span={12} md={6} xl={4}>
                   <Paper
                     h="190px"
@@ -281,120 +282,174 @@ export default function SearchResults({
                     m="xs"
                     radius="md"
                   >
-                    <Group noWrap>
-                      <Stack align="center" spacing={0}>
-                        <Container
-                          w={100}
-                          h={100}
-                          style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        >
-                          <img
-                            src={product.imgSrc}
-                            alt={product.name}
-                            style={{ width: "5rem" }}
-                          />
-                        </Container>
-                        <Container
-                          w={100}
-                          h={60}
-                          style={{
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                          }}
-                        >
-                          <img
-                            src={`/brand-logos/${product.shopName}.jpeg`}
-                            alt={product.shopName}
-                            style={{ width: "3rem" }}
-                          />
-                        </Container>
+                    <Flex
+                      gap="md"
+                      justify="flex-start"
+                      align="flex-start"
+                      direction="row"
+                      wrap="nowrap"
+                    >
+                      <Stack align="center">
+                        <Skeleton height={80} width={80} />
+                        <Skeleton height={60} width={60} />
                       </Stack>
 
                       <Stack
                         style={{
-                          width: "100%",
+                          width: "80%",
                         }}
-                        justify="space-between"
                       >
-                        <Box h={100}>
-                          <Text size={15} align="left" lineClamp={2}>
-                            {product.name}
-                          </Text>
-                          <Text fz="sm" c="dimmed">
-                            {product.shopName.charAt(0).toUpperCase() +
-                              product.shopName.slice(1).toLowerCase()}
-                          </Text>
-                          <Group spacing={0} align="center">
-                            <Text
-                              fz="md"
-                              c="cyan.7"
-                              sx={{
-                                cursor: "pointer",
-                                "&:hover": {
-                                  textDecoration: "underline",
-                                },
-                              }}
-                              fw={700}
-                              component="a"
-                              href={
-                                product.productUrl ? product.productUrl : ""
-                              }
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              Go to source
-                            </Text>
-                            <IconArrowBadgeRight
-                              size={20}
-                              style={{ color: "#1098AD" }}
-                            />
-                          </Group>
-                        </Box>
-
-                        <Group spacing={0} h={40} position="apart">
-                          <Text align="center">
-                            {product.price
-                              ? `€${product.price.toFixed(2)}`
-                              : "Price not available"}
-                          </Text>
-                          <Button
-                            variant="light"
-                            radius="xl"
-                            size="xs"
-                            leftIcon={<IconShoppingBagPlus />}
-                          >
-                            Add
-                          </Button>
+                        <Skeleton height={8} width={200} />
+                        <Skeleton height={8} width={160} />
+                        <Skeleton height={8} width={100} />
+                        <Space h={35} />
+                        <Group position="apart">
+                          <Skeleton height={25} width={35} />
+                          <Skeleton height={30} width={80} radius="lg" />
                         </Group>
                       </Stack>
-                    </Group>
+                    </Flex>
                   </Paper>
                 </Grid.Col>
               ))}
             </Grid>
-            {pages.totalPages && pages.totalPages > 0 ? (
-              <Pagination
-                mb={30}
-                py="xl"
-                spacing={5}
-                value={pages.activePage}
-                onChange={(p) => handlePageChange(p)}
-                total={pages.totalPages}
-              />
-            ) : null}
-          </Stack>
-        )
-      )}
+          ) : (
+            currentProducts &&
+            currentProducts.length > 0 && (
+              <Stack align="center" spacing={0}>
+                <Grid gutter={0} justify="center">
+                  {currentProducts.map((product, index) => (
+                    <Grid.Col key={index} span={12} md={6} xl={4}>
+                      <Paper
+                        h="190px"
+                        shadow="md"
+                        withBorder
+                        p="sm"
+                        m="xs"
+                        radius="md"
+                      >
+                        <Group noWrap>
+                          <Stack align="center" spacing={0}>
+                            <Container
+                              w={100}
+                              h={100}
+                              style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                              }}
+                            >
+                              <img
+                                src={product.imgSrc}
+                                alt={product.name}
+                                style={{ width: "5rem" }}
+                              />
+                            </Container>
+                            <Container
+                              w={100}
+                              h={60}
+                              style={{
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                              }}
+                            >
+                              <img
+                                src={`/brand-logos/${product.shopName}.jpeg`}
+                                alt={product.shopName}
+                                style={{ width: "3rem" }}
+                              />
+                            </Container>
+                          </Stack>
 
-      {queryParam &&
-        !loadingStates.loading &&
-        currentProducts &&
-        currentProducts.length === 0 && <>Sorry, there was nothing found!</>}
-    </Stack>
+                          <Stack
+                            style={{
+                              width: "100%",
+                            }}
+                            justify="space-between"
+                          >
+                            <Box h={100}>
+                              <Text size={15} align="left" lineClamp={2}>
+                                {product.name}
+                              </Text>
+                              <Text fz="sm" c="dimmed">
+                                {product.shopName.charAt(0).toUpperCase() +
+                                  product.shopName.slice(1).toLowerCase()}
+                              </Text>
+                              <Group spacing={0} align="center">
+                                <Text
+                                  fz="md"
+                                  c="cyan.7"
+                                  sx={{
+                                    cursor: "pointer",
+                                    "&:hover": {
+                                      textDecoration: "underline",
+                                    },
+                                  }}
+                                  fw={700}
+                                  component="a"
+                                  href={
+                                    product.productUrl ? product.productUrl : ""
+                                  }
+                                  target="_blank"
+                                  rel="noopener noreferrer"
+                                >
+                                  Go to source
+                                </Text>
+                                <IconArrowBadgeRight
+                                  size={20}
+                                  style={{ color: "#1098AD" }}
+                                />
+                              </Group>
+                            </Box>
+
+                            <Group spacing={0} h={40} position="apart">
+                              <Text align="center">
+                                {product.price
+                                  ? `€${product.price.toFixed(2)}`
+                                  : "Price not available"}
+                              </Text>
+                              <Button
+                                variant="light"
+                                radius="xl"
+                                size="xs"
+                                leftIcon={<IconShoppingBagPlus />}
+                              >
+                                Add
+                              </Button>
+                            </Group>
+                          </Stack>
+                        </Group>
+                      </Paper>
+                    </Grid.Col>
+                  ))}
+                </Grid>
+                {pages.totalPages && pages.totalPages > 0 ? (
+                  <Pagination
+                    mb={30}
+                    py="xl"
+                    spacing={5}
+                    value={pages.activePage}
+                    onChange={(p) => handlePageChange(p)}
+                    total={pages.totalPages}
+                  />
+                ) : null}
+              </Stack>
+            )
+          )}
+
+          {queryParam &&
+            !loadingStates.loading &&
+            currentProducts &&
+            currentProducts.length === 0 && (
+              <>Sorry, there was nothing found!</>
+            )}
+        </Stack>
+      </Tabs.Panel>
+
+      <Tabs.Panel value="basket" pt="xs">
+        Messages tab content
+      </Tabs.Panel>
+    </Tabs>
   );
 }
