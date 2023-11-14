afb4c27aec2257bfebc96c4eae858d8ff7ae3f4c
Dima Chavro <dchavro@gmail.com>
2023-11-14 18:00:50 +0000

    remove delete models

--- sw_core/core/admin.py
+++ sw_core/core/admin.py
@@ -3,6 +3,4 @@ import core.models as core_models
 
 # Register your models here.
 
-admin.site.register(core_models.CachedRelevantProduct)
-admin.site.register(core_models.CachedAllProduct)
 # admin.site.register(core_models.CachedProductsPage)
