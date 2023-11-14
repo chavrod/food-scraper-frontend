cd9b6a84efa1eaccbab49d63263893c946f1da49
Dima Chavro <dchavro@gmail.com>
2023-11-14 18:06:12 +0000

    Add basket related serilizers

--- sw_core/core/serializers.py
+++ sw_core/core/serializers.py
@@ -53,5 +53,30 @@ class CachedProductsPageSerializer(serializers.ModelSerializer):
         return internal_value
 
 
+@ts_interface()
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = core_models.Product
+        fields = ["name", "price", "imgSrc", "productUrl", "shop_name"]
+
+
+@ts_interface()
+class BasketItemSerializer(serializers.ModelSerializer):
+    product = ProductSerializer(read_only=True)
+
+    class Meta:
+        model = core_models.BasketItem
+        fields = ["product", "quantity"]
+
+
+@ts_interface()
+class BasketSerializer(serializers.ModelSerializer):
+    items = BasketItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = core_models.Basket
+        fields = ["id", "items"]
+
+
 if ENV == "DEV":
     generate_ts("../sw_customer/types/customer_types.ts")
