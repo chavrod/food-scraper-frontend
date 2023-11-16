650d76ec5c58a6ffd92632cb75dc68cf33b88f17
Dima Chavro <dchavro@gmail.com>
2023-11-16 17:35:55 +0000

    Register Basket routes

--- sw_core/core/urls.py
+++ sw_core/core/urls.py
@@ -1,16 +1,18 @@
-from django.urls import path
+from django.urls import path, include
 import core.views as core_views
 
+from rest_framework.routers import DefaultRouter
 
-# urlpatterns = router.urls
+
+# Create a router and register our viewsets with it.
+router = DefaultRouter()
+router.register(r"baskets", core_views.BasketViewSet, basename="basket")
+
 
 urlpatterns = [
-    # path("register/", core_views.RegisterView.as_view()),
-    # path("login/", core_views.LoginView.as_view()),
-    # path("user/", core_views.UserView.as_view()),
-    # path("logout/", core_views.LogoutView.as_view()),
     path(
         "cached_products_page/",
         core_views.CachedProductsPageViewSet.as_view({"get": "retrieve"}),
     ),
+    path("", include(router.urls)),
 ]
