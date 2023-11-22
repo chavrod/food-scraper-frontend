44cd5f03d003c46ed7d3b64c039d36e12ad9f8c1
Dima Chavro <dchavro@gmail.com>
2023-11-22 17:45:20 +0000

    add handler routes

---
+++ sw_customer/app/api/basketItemsApi.ts
@@ -0,0 +1,24 @@
+import apiClient from "./apiClient";
+
+const list = (data: {}) => apiClient.get("basket_items", data);
+
+const addItemQuantity = (data: {}) =>
+  apiClient.post("basket_items/add_item_quantity", data);
+
+const decreaseItemQuantity = (id: number, data: {}) =>
+  apiClient.post(`basket_items/${id}/decrease_item_quantity`, data);
+
+const removeProductItems = (id: number, data: {}) =>
+  apiClient.post(`basket_items/${id}/remove_product_items`, data);
+
+const clearAll = (data: {}) => apiClient.post("basket_items/clear_all", data);
+
+const basketItemsApi = {
+  list,
+  addItemQuantity,
+  decreaseItemQuantity,
+  removeProductItems,
+  clearAll,
+};
+
+export default basketItemsApi;
