4141b11cddba35041500146a37117744d598b0c3
Dima Chavro <dchavro@gmail.com>
2024-01-17 17:25:29 +0000

    refactor

--- sw_customer/app/page.tsx
+++ sw_customer/app/page.tsx
@@ -3,59 +3,51 @@ import SearchResults from "../Components/SearchResults";
 // Intenral Utils
 import getData from "@/app/api/getData";
 import customerApi from "@/app/api/customerApi";
-import { ScrapeStats, SearchParams, SearchMetaData } from "@/utils/types";
-import { Product } from "@/types/customer_types";
+import { ScrapeStats, SearchMetaData } from "@/utils/types";
+import { CachedProductsPage } from "@/types/customer_types";
 
 import "../styles/global.css";
 
-// TODO: Temporary
-type ProductData = {
-  results: Product[];
-  metadata: SearchMetaData;
-};
+export interface SearchParams {
+  query: string;
+  page: string;
+  is_relevant_only: boolean;
+}
 
-export default async function Home(outerParams: {
-  params: any;
+export default async function Home({
+  searchParams,
+}: {
   searchParams: SearchParams;
 }) {
-  let searchParams: SearchParams = {
-    query: outerParams.searchParams?.query || "",
-    page: outerParams.searchParams?.page || "1",
-    is_relevant_only: outerParams.searchParams?.is_relevant_only || true,
+  let params: SearchParams = {
+    query: searchParams?.query || "",
+    page: searchParams?.page || "1",
+    is_relevant_only: searchParams?.is_relevant_only || true,
   };
 
   const transformData = async (
     res: Response
-  ): Promise<ProductData | ScrapeStats> => {
+  ): Promise<CachedProductsPage | ScrapeStats> => {
     const jsonRes = await res.json();
 
     if (res.status === 206) {
       return { averageTimeSeconds: jsonRes.averageTimeSeconds };
     } else {
-      const mappedData: ProductData = {
-        results: jsonRes.results,
-        metadata: {
-          currentPage: jsonRes.page,
-          totalPages: jsonRes.total_pages,
-          keyword: jsonRes.query,
-          isRelevantOnly: jsonRes.is_relevant_only,
-        },
-      };
+      const mappedData: CachedProductsPage = jsonRes;
       return mappedData;
     }
   };
 
   const { data } = await getData<
-    ProductData | ScrapeStats,
+    CachedProductsPage | ScrapeStats,
     SearchParams
   >({
-    params: searchParams,
+    params: params,
     apiFunc: customerApi.getProducts,
     transformFunc: transformData,
   });
 
-  const products = data && "results" in data ? data.results : undefined;
-  const searchMetaData = data && "metadata" in data ? data.metadata : undefined;
+  const cachedProductsPage = data && "results" in data ? data : undefined;
   const averageScrapingTime =
     data && "averageTimeSeconds" in data ? data.averageTimeSeconds : undefined;
 
@@ -63,8 +55,7 @@ export default async function Home(outerParams: {
     <>
       <SearchResults
         searchText={searchParams.query}
-        products={products}
-        searchMetaData={searchMetaData}
+        cachedProductsPage={cachedProductsPage}
         averageScrapingTime={averageScrapingTime}
       />
     </>
