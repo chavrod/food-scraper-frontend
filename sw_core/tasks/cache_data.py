03372c42e9f753679ab40d51cf685e7a9db286a4
Dima Chavro <dchavro@gmail.com>
2024-05-06 12:20:28 +0100

    Save batch instacne

--- sw_core/tasks/cache_data.py
+++ sw_core/tasks/cache_data.py
@@ -1,19 +1,30 @@
 from typing import List, Dict
+from decimal import Decimal
 import os
 import sys
-import django
+from datetime import timedelta
 import argparse
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
+import time
+
 from celery import shared_task
+
+import django
 
 sys.path.append("/Users/dmitry/projects/shopping_wiz/sw_core")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop_wiz.settings")
 django.setup()
 
-from shop_wiz.settings import ENABLED_SCRAPERS
+from django.core.cache import cache
+from django.utils import timezone
+from django.db import transaction
+
+from shop_wiz.settings import (
+    ENABLED_SCRAPERS,
+    CACHE_SHOP_SCRAPE_EXECUTION_SECONDS,
+)
 import core.models as core_models
-import core.serializers as core_serializers
 from core.scraper_factory import ScraperFactory
 from core.scraper_factory.shop_scrapers import ShopScraper
 
@@ -21,11 +32,32 @@ from core.scraper_factory.shop_scrapers import ShopScraper
 factory = ScraperFactory()
 
 
+def begin_scraping(query_param):
+    cache_key = f"scrape_query_{query_param}"
+    last_update = cache.get(cache_key)
+
+    if last_update and (
+        (timezone.now() - last_update)
+        < timedelta(seconds=CACHE_SHOP_SCRAPE_EXECUTION_SECONDS)
+    ):
+        return
+    else:
+        # TODO: Rethink
+        is_relevant_only_param = True
+        cache_data.delay(query_param, is_relevant_only_param)
+        cache.set(
+            cache_key,
+            timezone.now(),
+            timeout=CACHE_SHOP_SCRAPE_EXECUTION_SECONDS,
+        )
+
+
 @shared_task
 def cache_data(query: str, is_relevant_only: bool):
     print(f"Passing to cache_data query: {query}; is_relevant_only: {is_relevant_only}")
 
     scraped_data = scrape_data(query, is_relevant_only)
+    print("data scraped")
     products = scraped_data["products"]
     if not products:
         return
@@ -72,23 +104,25 @@ def scrape_data(query: str, is_relevant_only: bool) -> Dict:
 
 
 def save_results_to_db(query, products_list):
-    products_to_create = []  # List to hold unsaved SearchedProduct instances
+    with transaction.atomic():
+        batch_instance = core_models.BatchUpload.objects.create(query=query)
 
-    for product in products_list:
-        product_instance = core_models.SearchedProduct(
-            query=query,
-            name=product["name"],
-            price=product["price"],
-            price_per_unit=product["price_per_unit"],
-            unit_type=product["unit_type"],
-            unit_measurement=product["unit_measurement"],
-            img_src=product["img_src"],
-            product_url=product["product_url"],
-            shop_name=product["shop_name"],
-        )
-        products_to_create.append(product_instance)
+        products_to_create = []
+        for product in products_list:
+            product_instance = core_models.SearchedProduct(
+                batch=batch_instance,
+                name=product["name"],
+                price=product["price"],
+                price_per_unit=product["price_per_unit"],
+                unit_type=product["unit_type"],
+                unit_measurement=product["unit_measurement"],
+                img_src=product["img_src"],
+                product_url=product["product_url"],
+                shop_name=product["shop_name"],
+            )
+            products_to_create.append(product_instance)
 
-    core_models.SearchedProduct.objects.bulk_create(products_to_create)
+        core_models.SearchedProduct.objects.bulk_create(products_to_create)
 
 
 if __name__ == "__main__":
