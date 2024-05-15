a630310d85ed451ad2bef97c2da6d5b2dca62749
Dima Chavro <dchavro@gmail.com>
2024-05-15 19:09:58 +0100

    add useless fact for the impatient

--- sw_customer/Components/CountdownCircle.tsx
+++ sw_customer/Components/CountdownCircle.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Link from "next/link";
-import { Paper, Text, Stack, Title } from "@mantine/core";
+import { Paper, Text, Stack, Title, Alert, Button } from "@mantine/core";
+import { IconBulbFilled } from "@tabler/icons-react";
 import { CountdownCircleTimer } from "react-countdown-circle-timer";
 // Intenral: Utils
 // Intenral: Components
@@ -15,19 +16,34 @@ export default function CountdownCircle({
   currentAverageScrapingTime,
   loading,
 }: CountdownCircleProps) {
+  const [uselessFact, setUselessFact] = useState(null);
+  const [loadingFact, setLoadingFact] = useState(false);
+
+  const fetchUselessFact = async () => {
+    setLoadingFact(true);
+    try {
+      const response = await fetch(
+        "https://uselessfacts.jsph.pl/api/v2/facts/random?language=en"
+      );
+      const data = await response.json();
+      setUselessFact(data.text);
+    } catch (error) {
+      console.error("Error fetching useless fact:", error);
+    }
+    setLoadingFact(false);
+  };
+
+  useEffect(() => {
+    fetchUselessFact();
+  }, []);
+
   return (
-    <Paper shadow="md" radius="md" p="md">
-      <Stack mt={20} mb={5} align="center">
-        <Title align="center" w={300}>
+    <Paper shadow="md" radius="md" p="sm">
+      <Stack mt={5} mb={5} align="center">
+        <Title order={2} align="center" w={300}>
           {" "}
           Searching supermarkets...
         </Title>
-        {currentAverageScrapingTime && (
-          <Text align="center" color="dimmed" w={300}>
-            Almost there! Gathering fresh supermarket data. Max seconds left...
-          </Text>
-        )}
-
         <Link href="/" legacyBehavior>
           <Text
             mt="xs"
@@ -57,6 +73,31 @@ export default function CountdownCircle({
             {useRenderTime}
           </CountdownCircleTimer>
         )}
+
+        <Alert
+          icon={<IconBulbFilled size="1rem" />}
+          title="Fun Useless Fact For the Impatient"
+          color="brand.7"
+          maw={290}
+        >
+          {loadingFact ? (
+            <Text>Loading your useless fact...</Text>
+          ) : uselessFact ? (
+            <Text>{uselessFact}</Text>
+          ) : (
+            <Text>Sorry, there was an issue loading your useless fact.</Text>
+          )}
+          <Button
+            mt={20}
+            onClick={() => {
+              if (loadingFact) return;
+              fetchUselessFact();
+            }}
+            loading={loadingFact}
+          >
+            Another Useless Fact
+          </Button>
+        </Alert>
       </Stack>
     </Paper>
   );
