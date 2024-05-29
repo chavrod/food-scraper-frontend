e113bb35a7a7eb0753206e965644deb5dd8fd2b4
Dima Chavro <dchavro@gmail.com>
2024-05-29 21:01:15 +0100

    change linux version to slim-bullseye

--- sw_core/docker-cmd.sh
+++ sw_core/docker-cmd.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 # vim:sw=4:ts=4:et
 
-su-exec "$USER" python manage.py collectstatic --noinput
+gosu "$USER" python manage.py collectstatic --noinput
 
 if [ "$1" = "--debug" ]; then
   # Django development server
-  exec su-exec "$USER" python manage.py runserver "0.0.0.0:$DJANGO_DEV_SERVER_PORT & /runsocket.sh"
+  exec gosu "$USER" python manage.py runserver "0.0.0.0:$DJANGO_DEV_SERVER_PORT & /runsocket.sh"
 else
   # Gunicorn
-  exec su-exec "$USER" gunicorn "$PROJECT_NAME.wsgi:application" \
+  exec gosu "$USER" gunicorn "$PROJECT_NAME.wsgi:application" \
     --bind "0.0.0.0:$GUNICORN_PORT" \
     --workers "$GUNICORN_WORKERS" \
     --timeout "$GUNICORN_TIMEOUT" \
