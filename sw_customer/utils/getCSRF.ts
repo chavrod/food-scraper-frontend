f7d60e38b04b4d32ca03558c1355c1802668622f
Dima Chavro <dchavro@gmail.com>
2024-12-31 15:28:34 +0300

    only query new csrf if cookie expired

--- sw_customer/utils/getCSRF.ts
+++ sw_customer/utils/getCSRF.ts
@@ -1,3 +1,10 @@
+const getCookie = (name: string): string | null => {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop()?.split(";").shift() || null;
+  return null;
+};
+
 const setCookie = (name: string, value: string, days?: number) => {
   let expires = "";
   if (days) {
@@ -9,23 +16,21 @@ const setCookie = (name: string, value: string, days?: number) => {
 };
 
 export default async function getClientSideCSRF(): Promise<string | null> {
+  const currentCSRF = getCookie("csrftoken");
+  if (currentCSRF) return currentCSRF;
+
   // Make a request to the Django endpoint
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}ping/`); // Adjust this to the path of your Django endpoint
-
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}ping/`);
   // Check if response is okay
-  if (!response.ok) {
-    throw new Error("Failed to get CSRF token from server");
-  }
-
+  if (!response.ok) throw new Error("Failed to get CSRF token from server");
   // Extract the CSRF token from the response body
   const data = await response.json();
   const { csrfToken } = data;
-
   if (!csrfToken) {
     throw new Error("Failed to get CSRF token");
   }
 
-  // Set CSRF token as a cookie
+  // Set new CSRF token as a cookie
   setCookie("csrftoken", csrfToken, 1); // The token will expire in 1 day
 
   return csrfToken;
